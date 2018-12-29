6cf06f90ef870e63d32a527c2dfef6914456150e
Ryan <ryan-price@uiowa.edu>
2018-12-28 23:30:04 -0600

    Cleaned up the addBoxes function

--- main.js
+++ main.js
@@ -1,62 +1,63 @@
 
 var ctx;
 var canvas;
+let cornerRadius = 100;
+let rectWidth;
+let rectHeight = 500;
+let rectWidthRadius;
+let rectHeightRadius;
+let rectY = 150;
+
 function setUp(){
     canvas = document.getElementById("canvas");
     canvas.height = innerHeight;
     canvas.width = innerWidth;
     ctx = canvas.getContext("2d");
+    rectWidth = (canvas.width/3 - 20);
+    rectWidthRadius = rectWidth - cornerRadius;
+    rectHeightRadius = rectHeight - cornerRadius;
     setInterval(buildClock,1000);
 }
 function buildClock() {
-    //ctx.rect(x,y,w,h);
 
-    addedBoxes(ctx);
+    addBoxes(ctx);
     addNumbers(ctx);
 }
-function addedBoxes(ctx) {
-    var cornerRadius = 100;
-    var rectY = 150;
-    var rectWidth = (canvas.width/3 - 20);
-    var rectHeight = 500;
+function addBoxes(ctx) {
 
     ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "white";
     ctx.lineJoin = "round";
     ctx.lineWidth = cornerRadius;
+    let yCorner = rectY+(cornerRadius/2);
 
-    ctx.strokeStyle = "white";
-    ctx.strokeRect(10+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-    ctx.rect(10+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-
-    ctx.strokeRect(((canvas.width/3) + 10 )+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-    ctx.rect(((canvas.width/3) + 10)+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-
-    ctx.strokeRect((2 * (canvas.width/3) + 10)+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-    ctx.rect((2 * (canvas.width/3) + 10)+(cornerRadius/2), rectY+(cornerRadius/2), rectWidth-cornerRadius, rectHeight-cornerRadius);
-
-    ctx.fillStyle = "white";
-    ctx.fill();
+    for(var i = 0; i<3; i++){
+        ctx.strokeRect(((i*(canvas.width/3)) + 10 )+(cornerRadius/2), yCorner, rectWidthRadius, rectHeightRadius);
+        ctx.rect(((i*(canvas.width/3)) + 10)+(cornerRadius/2), yCorner, rectWidthRadius, rectHeightRadius);
+    }
 
     ctx.fill();
 }
 
 function addNumbers(ctx) {
+
     ctx.font = (canvas.width/3)/2 +"px arial";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
-
+    var factor= rectWidthRadius/2;
     var now = new Date();
 
     var hour = now.getHours();
     hour %= 12;
 
-    ctx.fillText(hour.toString(),canvas.width/4 - 100, 425);
+    ctx.fillText(hour.toString(),(10+(cornerRadius/2)) + factor, 425);
 
     var min = now.getMinutes();
-    ctx.fillText(min.toString(), canvas.width/2, 425);
+    ctx.fillText(min.toString(), ((canvas.width/3) + 10 )+(cornerRadius/2) + factor, 425);
 
     var sec = now.getSeconds();
-    ctx.fillText(sec.toString(), 3 *canvas.width /4 + 100, 425);
+    ctx.fillText(sec.toString(), (2 * (canvas.width/3) + 10)+(cornerRadius/2)+ factor, 425);
 
 }
